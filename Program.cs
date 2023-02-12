27e9be8dd2557327b81595d831bc607d539dda6c
McQuile Karappen <mcquile@bbd.co.za>
2023-02-13 01:26:38 +0200

    filepath modification and code documentation

--- Program.cs
+++ Program.cs
@@ -1,44 +1,60 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.Text.RegularExpressions;
 
 
 namespace BirthdayExercise
 {
+    /// <summary>
+    /// Class to serve as an entry point and contain various utility methods.
+    /// </summary>
     class birthdayEvaluator
     {
+
+        /// <summary>
+        /// Entry point of application. Contains the main loop and flow of program.
+        /// Loops through ID numbers obtained by reading file, prints birthday derived from ID number and tracks birthdays occuring before and after 01/01/2010.
+        /// Finally writes the 2010 analysis to a file stored in the data directory.
+        /// </summary>
+        /// <param name="args">Command-Line arguments</param>
         static void Main(string[] args)
         {
-
             var before2010 = 0;
             var after2010 = 0;
 
-            var idArray = readFileAndReturnArray(@"C:\Users\bbdnet2874\Documents\C#\BirthdayExercise\Data\id.txt");
+            var idArray = ReadFileAndReturnArray("id.txt"); 
 
             foreach (var id in idArray)
             {
-                if (isIdValid(id))
+                if (IsIdValid(id))
                 {
-                    var birthDate = getDateFromID(id);
+                    var birthDate = GetDateFromId(id);
 
-                    if (isBirthdayValid(birthDate))
+                    if (IsBirthdayValid(birthDate))
                     {
-                        (before2010, after2010) = isBirthdayAfter2010((DateTime)birthDate, before2010, after2010);
-                        Console.WriteLine(getBirthdayFormat(birthDate.Value));
+                        (before2010, after2010) = IsBirthdayAfter2010((DateTime)birthDate, before2010, after2010);
+                        Console.WriteLine(GetBirthdayFormat(birthDate.Value));
                         continue;
                     }
                 }
-                Console.WriteLine($"Error: {id}");
+
+                var errorMessage = String.IsNullOrWhiteSpace(id) ? "EMPTY ENTRY" : id;
+                Console.WriteLine($"Error: {errorMessage}");
             }
-            writeToFile(before2010, after2010);
+            WriteToFile(before2010, after2010);
         }
 
-        protected static string[] readFileAndReturnArray(string fileName)
+        /// <summary>
+        /// Reads file and returns a string array with each element being a line from the file.
+        /// </summary>
+        /// <param name="fileName">File(path) name containing data to read</param>
+        /// <returns>(string[]) String array of each line in file</returns>
+        protected static string[] ReadFileAndReturnArray(string fileName)
         {
+            string filePath = GetDataPath() + fileName;
+
             try
             {
-                fileName = @"C:\Users\bbdnet2874\Documents\C#\BirthdayExercise\Data\id.txt";
-                return File.ReadAllLines(fileName);
+                return File.ReadAllLines(filePath);
             }
             catch (System.Exception)
             {
@@ -46,12 +62,33 @@ namespace BirthdayExercise
             }
         }
 
-        private static Boolean isIdValid(string id)
+        /// <summary>
+        /// Returns the file path of data directory in project
+        /// </summary>
+        /// <returns>string</returns>
+        protected static string GetDataPath()
+        {
+            var environment = System.Environment.CurrentDirectory;
+            return Directory.GetParent(environment).Parent.FullName.Replace(@"\bin", @"\Data\");
+
+        }
+
+        /// <summary>
+        /// Checks if a id(string) conforms to having a length of 13 characters with all being digits ranging from 0 to 9.
+        /// </summary>
+        /// <param name="id">String of id number to validate</param>
+        /// <returns>Boolean</returns>
+        private static Boolean IsIdValid(string id)
         {
             return Regex.IsMatch(id, "^\\d{13}$");
         }
 
-        private static DateTime? getDateFromID(string id)
+        /// <summary>
+        /// Uses the first 6 characters from the id(string) to generate and return a DateTime object. Will return null should a format exception be thrown during parsing.
+        /// </summary>
+        /// <param name="id">String of id number</param>
+        /// <returns>DateTime or null</returns>
+        private static DateTime? GetDateFromId(string id)
         {
             var birthdaySequence = id.Substring(0, 6);
             try
@@ -64,17 +101,34 @@ namespace BirthdayExercise
             }
         }
 
-        private static Boolean isBirthdayValid(DateTime? birthday)
+        /// <summary>
+        /// Checks if nullable DateTime argument passed is before the current date and is not null.
+        /// </summary>
+        /// <param name="birthday">DateTime nullable of birthday</param>
+        /// <returns>Boolean</returns>
+        private static Boolean IsBirthdayValid(DateTime? birthday)
         {
             return birthday != null && DateTime.Now.CompareTo(birthday) > 0;
         }
 
-        private static String getBirthdayFormat(DateTime birthday)
+        /// <summary>
+        /// Converts the DateTime argument to the desired string format and returns the resultant string.
+        /// </summary>
+        /// <param name="birthday">DateTime object</param>
+        /// <returns>String</returns>
+        private static String GetBirthdayFormat(DateTime birthday)
         {
             return birthday.ToString("dd/MM/yyyy");
         }
 
-        private static (int, int) isBirthdayAfter2010(DateTime birthday, int before2010, int after2010)
+        /// <summary>
+        /// Checks if the DateTime argument passed is before or after 01/01/2010. Updates the relevent integer and returns the values.
+        /// </summary>
+        /// <param name="birthday">DateTime object containing date to compare</param>
+        /// <param name="before2010">int used to track how many dates are before 01/01/2010</param>
+        /// <param name="after2010">int used to track how many dates are after 01/01/2010</param>
+        /// <returns></returns>
+        private static (int, int) IsBirthdayAfter2010(DateTime birthday, int before2010, int after2010)
         {
             var newYears2010 = new DateTime(2010, 01, 01);
 
@@ -83,15 +137,22 @@ namespace BirthdayExercise
             return (before2010, after2010);
         }
 
-        private static void writeToFile(int before2010, int after2010)
+        /// <summary>
+        /// Receives two integer arguments tracking birthdays before and after 01/01/2010 and writes the information to a file called "2010Analysis.txt" in the Data directory.
+        /// </summary>
+        /// <param name="before2010">int used to track how many dates are before 01/01/2010</param>
+        /// <param name="after2010">int used to track how many dates are after 01/01/2010</param>
+        private static void WriteToFile(int before2010, int after2010)
         {
+            var fileName = GetDataPath() + "2010Analysis.txt";
+
             string[] lines =
             {
                 $"Number of people born before 01/01/2010: {before2010}",
                 $"Number of people born after 01/01/2010: {after2010}"
             };
 
-             File.WriteAllLines(@"C:\Users\bbdnet2874\Documents\C#\BirthdayExercise\Data\2010Analysis.txt", lines);
+             File.WriteAllLines(fileName, lines);
         }
     }
 }
